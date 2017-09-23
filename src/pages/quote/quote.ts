c9b85f5dc89e7d9c3d22bdd514ac41cd4422c886
irhamputra <irhamputraprasetyo@gmail.com>
2017-09-23 14:03:08 +0200

    passing data to modal and dismiss modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import {ViewController} from "ionic-angular";
+import {ViewController, NavParams} from "ionic-angular";
 
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+  person: string;
+  text: string;
 
-  constructor(public viewCtrl: ViewController) {
+  constructor(public viewCtrl: ViewController, private navParams: NavParams) {}
+
+  ionViewDidLoad(){
+    this.person = this.navParams.get('person');
+    this.text = this.navParams.get('text');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+  onClose(){
+    this.viewCtrl.dismiss();
   }
-
 }
