bbf2949e0a5ed0dc68b4568d862743b4f4356960
irhamputra <irhamputraprasetyo@gmail.com>
2017-09-23 18:26:40 +0200

    Add sliding to the item list and delete alert task

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 import {Quote} from "../../data/quote.interface";
 import {QuotesService} from "../../services/quotes";
-import {ModalController} from "ionic-angular";
-import {QuotePage} from '../quote/quote';
+import {ModalController, AlertController} from "ionic-angular";
+import {QuotePage} from "../quote/quote";
 
 @Component({
   selector: 'page-favorites',
@@ -11,28 +11,52 @@ import {QuotePage} from '../quote/quote';
 export class FavoritesPage {
   quotes: Quote[];
 
-  constructor(private quoteService: QuotesService, private modalCtrl: ModalController){}
+  constructor(private quoteService: QuotesService,
+              private modalCtrl: ModalController,
+              private alertCtrl: AlertController) {
+  }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.quotes = this.quoteService.getFavorite();
   }
 
-  onViewQuote(quote: Quote){
+  onViewQuote(quote: Quote) {
     const modal = this.modalCtrl.create(QuotePage, quote);
     modal.present();
     modal.onDidDismiss((remove: boolean) => {
-      if (remove){
-        this.quoteService.removeFromFavorite(quote);
-
-        // #1 simple reload page after unfavorite
-        // this.quotes = this.quoteService.getFavorite();
-
-        // #2 Advance reload page after unfavorite
-        const position = this.quotes.findIndex((quoteElement: Quote) => {
-          return quoteElement.id == quote.id
-        });
-        this.quotes.splice(position, 1);
+      if (remove) {
+        this.onRemoveFromFavorite(quote);
       }
     });
+  }
+
+  onRemoveFromFavorite(quote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Delete Quotes',
+      message: 'Are you sure want to delete your quote?',
+      buttons: [{
+        text: 'Yes',
+        handler: () => {
+          this.quoteService.removeFromFavorite(quote);
+
+          // #1 simple reload page after unfavorite
+          // this.quotes = this.quoteService.getFavorite();
+
+          // #2 Advance reload page after unfavorite
+          const position = this.quotes.findIndex((quoteElement: Quote) => {
+            return quoteElement.id == quote.id
+          });
+          this.quotes.splice(position, 1);
+        }
+      },
+        {
+          text:'No',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel Delete Task');
+          }
+        }]
+    });
+    alert.present();
   }
 }
