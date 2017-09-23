ebb9a17b9ddb9ac720e254368fd22bacfef4076e
irhamputra <irhamputraprasetyo@gmail.com>
2017-09-23 10:43:25 +0200

    adding event handler

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,29 +1,48 @@
-import {Component, OnInit} from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import {Component, OnInit} from "@angular/core";
+import {NavParams, AlertController} from "ionic-angular";
 import {Quote} from "../../data/quote.interface";
 
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage implements OnInit{
-  quoteGroup: {category: string, quotes: Quote[], icon:string};
+export class QuotesPage implements OnInit {
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(public navParams: NavParams) {
+  constructor(public navParams: NavParams, private alertCrtl: AlertController) {
   }
 
   // with OnInit LifeCycle
-  ngOnInit(){
+  ngOnInit() {
     this.quoteGroup = this.navParams.data;
   }
 
-  onAddToFavorite(quote:string){
-    console.log(quote);
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCrtl.create({
+      title: 'Add Quotes',
+      message: 'Are you sure?',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          console.log('Ok');
+          console.log(selectedQuote)
+        }
+      },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled')
+          }
+        }]
+    });
+
+    alert.present();
   }
 
   // with Elvis Operator (?)
   // ionViewDidLoad() {
-    // this.quoteGroup = this.navParams.data;
+  // this.quoteGroup = this.navParams.data;
   // }
 
 }
